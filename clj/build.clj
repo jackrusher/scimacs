c2ebfe2f593f4bcd1c9428dfc34e12fd1f0ef891
Michiel Borkent <michielborkent@gmail.com>
2023-03-20 13:23:33 +0100

    Streamline native build

--- clj/build.clj
+++ clj/build.clj
@@ -4,19 +4,28 @@
 (def lib 'com.rusher/scimacs)
 (def version (format "0.1.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
-(def basis (b/create-basis {:project "deps.edn"}))
+(def uber-file "target/uber.jar")
+(def basis (b/create-basis {:project "deps.edn" :aliases [:native :native-22.3.1]}))
 
 (defn clean [_]
   (b/delete {:path "target"}))
 
 (defn libscimacs [_]
   (clean nil)
+  (println "Compiling clojure...")
   (b/compile-clj {:basis basis
                   :src-dirs ["src"]
                   :class-dir class-dir
                   :jvm-opts ["-Dclojure.compiler.direct-linking=true"
-                             "-Dclojure.spec.skip-macros=true"]})
+                             "-Dclojure.spec.skip-macros=true"]
+                  :ns-compile '[com.rusher.scimacs]})
 
+  (println "Compiling java...")
   (b/javac {:src-dirs ["src"]
             :class-dir class-dir
-            :basis basis}))
+            :basis basis})
+
+  (println "Creating uberjar:" uber-file)
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis basis}))
